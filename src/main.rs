ea2bdb1f08be42250d12859afc010401cc1604a8
Camilo Lozano <camilozano98@gmail.com>
2020-09-09 19:17:12 -0400

    Implemented faster alg

--- src/main.rs
+++ src/main.rs
@@ -3,44 +3,57 @@ use std::sync::mpsc;
 use std::thread;
 use std::time::Instant;
 
-static NTHREADS: i32 = 8;
-static UPPER_LIMIT: usize = 100000000;
+static NTHREADS: i32 = 64;
+static UPPER_LIMIT: i32 = 100000000;
 
-fn sieve(from:usize, to:usize, tx: Sender<usize>) -> i32 {
+fn block_sieve(from: i32, to: i32, tx: Sender<usize>)-> i32 {
 
-    let mut is_prime = vec![true; to+1];
-    let sqrtlmt = (to as f64).sqrt() as usize +1;
+    let memory_size = ((to-from+1)/2) as usize;
+    let mut is_prime: Vec<bool> = vec![true;memory_size];
 
-    let mut min = from;
-    if to > 2 {
-        is_prime[0] = false;
-        is_prime[1] = false;
-        min = 2;
-    }
+    // Maybe +1 check line 11
+    let range = (to as f64).sqrt() as i32 + 1;
+    
+    for i in (3..range).step_by(2){
+        if i >= 3*3 && i%3 == 0 {continue}
+        if i >= 5*5 && i%5 == 0 {continue}
+        if i >= 7*7 && i%7 == 0 {continue}
+        if i >= 11*11 && i%11 == 0 {continue}
+        if i >= 13*13 && i%13 == 0 {continue}
+        
+        let mut min = ((from+i-1)/i)*i;
+        if min < i*i {min = i*i}
+        if (min & 1) == 0 {min += i}
 
-    for idx in min..sqrtlmt {
-        if is_prime[idx]{
-            let mut multiple = idx*idx;
-            while multiple <= to {
-                is_prime[multiple] = false;
-                multiple += idx;
-            }
+        let mut j = min;
+        while j <= to{
+            let index = ((j-from)/2) as usize;
+            is_prime[index] = false;
+            j+= 2*i;
         }
+
+
     }
 
-    let mut count = 0;
-    for i in from..is_prime.len() {
+    let mut found = 0;
+    let idx_fix = ((from&1)+1)%2;
+
+    // let mut prime_list: Vec<i32> = Vec::new();
+    for i in 0..memory_size {
         if is_prime[i] {
-            count += 1;
-            tx.send(i).unwrap();
+            found += is_prime[i] as i32;
+            let actual_number = from + 2*(i as i32) + idx_fix;
+            tx.send(actual_number as usize).unwrap();
+            // prime_list.push(actual_number);
         }
     }
-    count
+
+    found
 }
 
-fn prime_runner(upper: usize) -> u32 {
+fn prime_runner(upper: i32) -> u32 {
 
-    let div = upper/(NTHREADS as usize);
+    let div: i32 = upper/NTHREADS;
     let mut count: u32 = 0;
     let mut sum: u64 = 0;
 
@@ -49,13 +62,13 @@ fn prime_runner(upper: usize) -> u32 {
 
     let start = Instant::now();
 
-    for i in (0..upper).step_by(div){
+    for i in (0..upper).step_by(div as usize){
         let thread_tx = tx.clone();
-        let from = i;
+        let from: i32 = i as i32;
         let to = i+div-1;
 
         let child = thread::spawn(move || {
-            sieve(from,to,thread_tx);
+            block_sieve(from,to,thread_tx);
             // println!("{}\tThread {} finished", count,i);
         });
         children.push(child);
